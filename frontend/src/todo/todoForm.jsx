0f0d874351f80a270e052e857f5efc2bbd9bad64
root <root@localhost.localdomain>
2019-01-24 23:21:11 -0200

    1.0.6

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <input type="text" className="input" value="FORM" />
+  </div>
+);
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default props => (
+  <div>
+    <h1>Lista</h1>
+    <li>
+      <ul>item 1</ul>
+      <ul>item 2</ul>
+      <ul>item 3</ul>
+    </li>
+  </div>
+);
